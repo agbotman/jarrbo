d64ef7ae56b6befd9fa9618206837c0a7e0510c7
Ton Botman <ton@jarrbo.nl>
2024-12-05 14:29:53 +0100

    Add REMOTE_BACKUP and DATA_FOLDER to settings to support backup

--- settings.py
+++ settings.py
@@ -166,3 +166,6 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REMOTE_BACKUP = Path(LOCALS['REMOTE_BACKUP'])
+DATA_FOLDER = Path(LOCALS['DATA_FOLDER'])
